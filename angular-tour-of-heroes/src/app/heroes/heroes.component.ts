bdc3b848b4d29fc20abd6470f433505ec14a1590
Cecilia Carlon <ceciliacarlon2@gmail.com>
2022-09-03 18:58:43 -0300

    Se agregan los capitulos 3 y 4 completos del tour

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +12,20 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   //Variables
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService : MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-
-  onSelect(hero : Hero){
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+  onSelect(hero : Hero) : void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 }
